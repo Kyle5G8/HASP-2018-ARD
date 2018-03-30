ddd206601d9b5e482fafc023ab03497af48d660f
Kyle5G8 <34083234+Kyle5G8@users.noreply.github.com>
2018-03-30 10:28:07 -0700

    Update Secondary.ino

--- Secondary.ino
+++ Secondary.ino
@@ -1,10 +1,26 @@
-int led = 13;
-void setup()
-{
-  pinMode(led, OUTPUT);
+void setup() {
+    Serial.begin(9600);
 }
-
-void loop()
-{
-digitalWrite(led, HIGH);
+ 
+void loop(){
+    float pressure = readPressure(A14);
+    float millibars = pressure/100;
+ 
+    Serial.println();
+    Serial.print("Pressure = ");
+    Serial.print(pressure);
+    Serial.println(" pascals");
+    Serial.print("Pressure = ");
+    Serial.print(millibars);
+    Serial.println(" millibars");
+    delay(1000);
+}
+ 
+/* Reads pressure from the given pin.
+* Returns a value in Pascals
+*/
+float readPressure(int pin){
+    int pressureValue = analogRead(pin);
+    float pressure=((pressureValue/1024.0)+0.095)/0.000009;
+    return pressure;
 }
