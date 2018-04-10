ec87f5010977dc43a8156ec169197c2938993d8a
Kyle5G8 <34083234+Kyle5G8@users.noreply.github.com>
2018-04-10 09:12:54 -0700

    Update MainSketch.ino

--- MainSketch.ino
+++ MainSketch.ino
@@ -1,3 +1,4 @@
+String SerialIn;
 void setup() 
 {
 Serial.begin(9600);
@@ -31,4 +32,29 @@ float readHumidity() //RH
  float RH = rhRead/(1.0546-0.00216*23);// replace 23 with thermistor value for temperature compensation
  float rhvoltage = (5*(0.00636*RH+0.1515));
  return rhvoltage*3;
+}
+
+void checkCommands
+{
+ while(Serial.available())
+ {
+ SerialIn = Serial.ReadString();
+ }
+ if(filterCommand(SerialIn))
+ {
+  //command actions
+ }
+ else if(!filterCommand(SerialIn))
+ {
+ return;
+ }
+}
+
+bool filterCommand(String a)
+{
+ if(a.charAt(0) == '2' && a.charAt(2) == '2')
+ {
+   return true;
+ }
+ else return false;
 }
